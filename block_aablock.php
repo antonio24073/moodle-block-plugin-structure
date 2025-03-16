836ca37ff720a8b75abb6a8a7ea2dee9892539ed
Antonio Augusto <antonio.etec.inf5@gmail.com>
2025-03-16 13:26:21 -0300

    first commit

---
+++ block_aablock.php
@@ -0,0 +1,42 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+use core_external\util as external_util;
+
+/**
+ * Form for editing HTML block instances.
+ *
+ * @package   block_aablock
+ * @copyright 1999 onwards Martin Dougiamas (http://dougiamas.com)
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class block_aablock extends block_base {
+    function init() {
+        $this->title = get_string('pluginname', 'block_aablock');
+    }
+
+    function get_content() {
+        global $CFG;
+
+        if ($this->content !== NULL) {
+            return $this->content;
+        }
+
+        $this->content->text = get_string('text','block_aablock');
+        $this->content->footer = get_string('footer','block_aablock');
+        return $this->content;
+    }
+}
